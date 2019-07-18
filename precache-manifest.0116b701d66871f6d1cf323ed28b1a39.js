74413ac1435db822a3f6499927b088c27ce7d1f2
satheeshdragon <satheeshdragon.satheesh@gmail.com>
2019-07-18 22:58:22 +0530

    Updates

---
+++ precache-manifest.0116b701d66871f6d1cf323ed28b1a39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7e8487edd2fbe7f9fc68fb55c42801",
+    "url": "/clipboard_firebase/index.html"
+  },
+  {
+    "revision": "b43e4e8d0659efc9dcfb",
+    "url": "/clipboard_firebase/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "f639501e4bead351c5f9",
+    "url": "/clipboard_firebase/static/js/2.fb32ec7d.chunk.js"
+  },
+  {
+    "revision": "b43e4e8d0659efc9dcfb",
+    "url": "/clipboard_firebase/static/js/main.8efc599e.chunk.js"
+  },
+  {
+    "revision": "9b81b23740d17ba52d11",
+    "url": "/clipboard_firebase/static/js/runtime~main.9b4deaa2.js"
+  }
+]);
